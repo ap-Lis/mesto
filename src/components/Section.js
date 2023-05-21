19d995e0c6beef6b5ee0e0e9bd4d9dfcfea6bed1
Gleb <gleb@grib.in>
2023-05-21 23:00:09 +0300

    small fixes

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,12 +1,11 @@
 export class Section {
-    constructor({items, renderer}, containerSelector) {
+    constructor({renderer}, containerSelector) {
       this._container = document.querySelector(containerSelector);;
-      this._items = items;
       this._renderer = renderer;
     }
   
-    renderAllElements() {
-      this._items.forEach(this._renderer);
+    renderAllElements(items) {
+      items.forEach(this._renderer);
     }
   
     addItem(itemHtml) {
